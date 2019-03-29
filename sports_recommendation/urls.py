5254b0828c7fc622f6f3eb386b586cab858c0ce0
Shawn-Philippe Levasseur <levasseur.sp@gmail.com>
2019-03-29 14:58:25 -0400

    Instead of saving everytime, I did get_or_create

--- sports_recommendation/urls.py
+++ sports_recommendation/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import get_locations
 
 urlpatterns = [
-    path('', get_locations, name='sports_recommendation'),
+    path('', get_locations, name='locations'),
 ]
--- sports_recommendation/views.py
+++ sports_recommendation/views.py
@@ -8,13 +8,14 @@ requests_cache.install_cache(
     'amilia_cache', backend='sqlite', expire_after=24*60*60
 )
 
+
 def get_locations(request, lng=45.5035, lat=-73.5685, radius=10):
     r = requests.get(
         'https://www.amilia.com/api/v3/fr/locations?type=Radius&coordinates=%f,%f&radius=%i'
         % (lng, lat, radius))
 
     for location in r.json()['Items']:
-        Location(
+        Location.objects.get_or_create(
             locationId=location['Id'],
             name=location['Name'],
             fullName=location['FullName'],
@@ -24,9 +25,9 @@ def get_locations(request, lng=45.5035, lat=-73.5685, radius=10):
             parentId=location['ParentId'],
             topParentId=location['TopParentId'],
             ancestorIds=location['AncestorIds'],
-            address=location['Address']['Address1'],
+            address=location['Address'],
             keywords=location['Keywords'],
-        ).save()
+        )
 
     return JsonResponse(r.json(), safe=False)
 
